18793ca0d8de57be3f994504e8d619159cb0e629
Oleksandr Varga <vargasasha@gmail.com>
2023-09-06 13:30:36 +0300

    add files

---
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,13 @@
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+
+    <div>
+      {options.map(option => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+
+};
---
+++ src/components/Layout.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Layout = styled.main`
+   padding: 0 15px;
+  max-width: 900px;
+  margin: 0 auto;
+`;
