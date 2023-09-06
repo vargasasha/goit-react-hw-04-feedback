6688b842d22cab75c753f55f573b91286af763b6
Oleksandr Varga <vargasasha@gmail.com>
2023-09-06 15:14:41 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,10 @@ import { Notification } from './Notification/Notification';
 import { useState } from 'react';
 
 export const App = () => {
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [positivePercentage, setPositivePercentage] = useState(0);
 
   const handleClick = type => {
     switch (type) {
@@ -35,7 +35,7 @@ export const App = () => {
   };
 
   const countPositiveFeedbackPercentage = () => {
-    return Math.round((good / countTotalFeedback) * 100 || 0);
+    return Math.round((good / countTotalFeedback()) * 100 || 0);
   };
 
   return (
